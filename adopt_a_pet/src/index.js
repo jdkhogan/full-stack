2c5fe42a8c381ba08e3ddb264489e6a239cb0292
jdkhogan <jahoga@gmail.com>
2023-05-20 12:59:39 -0500

    adopt a pet homepage

--- adopt_a_pet/src/index.js
+++ adopt_a_pet/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Outlet } from 'react-router-dom';
+
 
 const { worker } = require('./mocks/browser');
 worker.start();
